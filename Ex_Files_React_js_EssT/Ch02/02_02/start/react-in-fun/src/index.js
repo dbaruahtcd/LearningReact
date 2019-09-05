249fdfe1788f2a37be7669195161d4bea57d6270
Dan <dibangku.baruah@sage.com>
2019-09-05 16:41:01 +0100

    Created a library app to list books and also made use of state

--- Ex_Files_React_js_EssT/Ch02/02_02/start/react-in-fun/src/index.js
+++ Ex_Files_React_js_EssT/Ch02/02_02/start/react-in-fun/src/index.js
@@ -1,47 +1,55 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-let tennisData = {
-	total: 60,
-	hp: 2,
-	ucd: 10,
-	goal: 300
+
+const BookList = [
+	{title: "Lessons from History", author: "Durant", pages: '99'},
+	{title: "Sapiens", author: "Harrari", pages: '399'},
+	{title: "Man's search for meaning", author: "Victor Frank", pages: '189'},
+	{title: "Super Intelligence", author: "Nick Bostrom", pages: "320"}
+]
+
+// const Library = ({books}) => {
+class Library extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			open: open
+		}
+	}
+
+	render() {
+		console.log(this.state)
+		const  { books } = this.props
+		return (
+			<div>
+				<h1>The library is: {this.state.open ? 'open' : 'closed'} </h1>
+				{books.map(
+					(book, i) => 
+						<Book 
+							key={i}
+							title={book.title} 
+							author={book.author} 
+							pages={book.pages} />
+				)}
+			</div>
+		)
+	}
 }
 
-const TennisDayCounter = ({total, hp, ucd, goal}) => {
-	return (
+
+
+const Book = ({title, author, pages}) => {
+	return(
 		<section>
-			<div>
-				<p> Total days: {total} </p>
-			</div>
-			<div>
-				<p> Herbert Park: {hp} </p>
-			</div>
-			<div>
-				<p> UCD: {ucd} </p>
-			</div>
-			<div>
-				<p> Goals Progress: {calcGoalProgress(total, goal)} </p>
-			</div>
+			<h2> {title} </h2>
+			<p> by: {author} </p>
+			<p> Pages: {pages} pages </p>
 		</section>
 	)
 }
 
-const calcGoalProgress = (total, goal) => {
-		return getPercent(total/goal);
-}
-
-const	getPercent = decimal => {
-		return decimal * 100 + '%';
-}
-
 render(
-	<TennisDayCounter 
-		total={tennisData.total}
-		hp={tennisData.hp}
-		ucd={tennisData.ucd}
-		goal={tennisData.goal}
-	/>,
+	<Library books={BookList}/>,
 	document.getElementById('root')
-	)
-
+)
